2e86b0a57454d4609d1ee0c86e10282061eb24c9
Josh Allen <beefzilla@gmail.com>
2018-01-24 10:50:25 -0500

    V1 (#1)
    * adding bower.json
    
    * adding package.json
    
    * Added autoloader

--- backbone.waiter.js
+++ backbone.waiter.js
@@ -269,6 +269,15 @@
 
     });
 
+    Waiter.AutoLoader = function (View, props) {
+        if (View) {
+          var myView = new View(props)
+          if ($(myView.el).length > 0) {
+            // hook up ui and events
+            myView.render()
+          }
+        }
+      }
 
     return Waiter;
 });
