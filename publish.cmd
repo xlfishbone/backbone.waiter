9e8037de94208ac436be39de575dbedafa0ad6db
xlfishbone <beefzilla@gmail.com>
2018-01-25 10:01:38 -0500

    update publish

--- publish.cmd
+++ publish.cmd
@@ -1,28 +1,20 @@
 @echo off
 
-goto getVersionNumber
-set /p doNpm=Update package.json and push to npm: y or n?
-IF "%doNpm%"=="y" GOTO npm 
-
-set /p doGit=Tag and Push to git: y or n?
-IF "%doGit%"=="y" GOTO git 
-
-:getVersionNumber
+REM Get version number
 set /p ver=New Version Number or q to quit:
 IF "%ver%"=="q" GOTO end
 IF "%ver%"=="" GOTO Error 
 
-:git
-git tag v%ver%
-git push
-git push --tags
-
-:npm
+REM update npm with new version and publish
 npm version %ver%
 npm publish
 
+REM tag and push to origin
+git tag v%ver%
+git push origin v%ver%
+
 :Error
 echo no version number found! 
-goto getVersionNumber
+goto start
 
 :end
